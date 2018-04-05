ca15a28ffa7fb71d063692e234e4b54a9015b0dd
Marc Foley <m.foley.88@gmail.com>
2018-04-05 16:51:31 +0100

    Use presupplied ttfs instead

--- regression-tests/generate_diffs.py
+++ regression-tests/generate_diffs.py
@@ -1,49 +1,18 @@
 """
 Compare TN's Roboto visually against the official version.
 
-
-Use fonttool's varLib mutator to generate the static instances
-for comparison.
-
 Script relies on Browserstack. Its very slow.
 """
 import os
 import json
 from glob import glob
 
-from fontTools.varLib import mutator
-from fontTools.ttLib import TTFont
 from diffbrowsers.utils import load_browserstack_credentials
 from diffbrowsers.diffbrowsers import DiffBrowsers
 
 
-AXIS = {
-    'Thin': {'wght': 100, 'wdth': 100},
-    'Regular': {'wght': 400, 'wdth': 100},
-    'Black': {'wght': 900, 'wdth': 100},
-    # TODO Add all instances
-}
-
-
-def generate_ttf_instances(vf_font, dst):
-    """Generate instances from a variable font"""
-
-    vf_font = TTFont(vf_font)
-    for style in AXIS:
-        font = mutator.instantiateVariableFont(vf_font, AXIS[style])
-
-        filename = "Roboto-{}.ttf".format(style)
-        file_dst = os.path.join(dst, filename)
-        if os.path.isfile(file_dst):
-            os.remove(file_dst)
-        font.save(file_dst)
-
-
 def main():
     c_dir = os.path.dirname(__file__)
-    vf_font = os.path.join(c_dir, 'roboto-tn', 'src', 'Roboto-VF.ttf')
-    instances_dir = os.path.join(c_dir, 'roboto-tn')
-    generate_ttf_instances(vf_font, instances_dir)
 
     imgs_dir = os.path.join(c_dir, 'imgs')
 
